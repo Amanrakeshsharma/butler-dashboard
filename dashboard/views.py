d6cf25127eadedf78796bc503f1b2c44fa438c1e
Aman Sharma <amanrakeshsharma2003@gmail.com>
2026-03-30 13:42:35 +0530

    initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'dashboard/home.html')
+def dashboard_page(request):
+    return render(request, 'dashboard/dashboard.html')
